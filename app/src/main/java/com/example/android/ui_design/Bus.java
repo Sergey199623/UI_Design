c9f5f199e0b96ba01dccc2582d7a12ee528aac6b
Sergey <belyakovsergeyrus@yandex.ru>
2020-06-03 22:59:44 +0300

    Update by 03.06.2020

---
+++ app/src/main/java/com/example/android/ui_design/Bus.java
@@ -0,0 +1,14 @@
+package com.example.android.ui_design;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Bus extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bus);
+    }
+}
--- app/src/main/java/com/example/android/ui_design/MainActivity.kt
+++ app/src/main/java/com/example/android/ui_design/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.android.ui_design
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var drawerLayout : DrawerLayout
+    lateinit var navigationView : NavigationView
+    lateinit var toolbar: Toolbar
+//    lateinit var binding :
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
 }
